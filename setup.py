af9704700b8ed5a60663bc783cb1f4d3029c5d39
Jeroen Clemens Vermunt <39826623+jeroenvermunt@users.noreply.github.com>
2024-02-22 10:16:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ from setuptools import setup
 if __name__ == '__main__':
     setup(
         package_data={
-            'pysdk': ['method_template.txt'],
+            'pysdk': [
+                'method_template.jinja',
+                'import_template.jinja',
+                'body_template.jinja'
+            ],
         }
     )
